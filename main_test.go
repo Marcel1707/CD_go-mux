fa0cc95720a0bd6ef25dc2666e55bed21b65bb4f
Marcel Ciesla <S2110455024@students.fh-hagenberg.at>
2022-05-25 15:45:49 +0200

    remove hardcoded db credentials

--- main_test.go
+++ main_test.go
@@ -15,15 +15,10 @@ import (
 var a main.App
 
 func TestMain(m *testing.M) {
-	// a.Initialize(
-	// 	os.Getenv("APP_DB_USERNAME"),
-	// 	os.Getenv("APP_DB_PASSWORD"),
-	// 	os.Getenv("APP_DB_NAME"))
-
 	a.Initialize(
-		"postgres",
-		"secret",
-		"postgres")
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
 
 	ensureTableExists()
 	code := m.Run()
